10635061679eca46f0298f17c7cc23149e2f5ea2
nehalnaveed1993 <148252844+nehalnaveed1993@users.noreply.github.com>
2023-10-20 21:56:56 +0000

    Updated

---
+++ Working/01_02_variables.js
@@ -0,0 +1,10 @@
+const cusid = 12;
+let cusname = "Nehal Naveed";
+var cusaddress = "Johar";
+email  = 'nehalnaveed@gmail.com';
+let orderid;
+
+// cusid= 33;
+
+console.log(cusid);
+console.table([cusid,cusname,cusaddress,orderid]);
