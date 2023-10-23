307638064997f40b5f15307597c40a9b9b234398
nehalnaveed1993 <148252844+nehalnaveed1993@users.noreply.github.com>
2023-10-23 19:31:27 +0000

    Updated 24-10-2023

---
+++ Working/01_03_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //Treat All code as newer version of Javascript
+
+//alert("Hello"); //We are using Node JS not browser
+
+console.log(
+    1
+    +
+    4
+)   //Code Readability Should be high
+
+
+let user_id= 123;
+let user_name = "Nehal";
+let user_status = true;
+let user_state;
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean  => true/false
+//null => standalone value
+//undefined 
+//symbol => unique
+
+console.log(typeof "Ali");
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
