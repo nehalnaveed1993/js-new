3ddb196923fe344c481f41357608c69620737cdd
nehalnaveed1993 <148252844+nehalnaveed1993@users.noreply.github.com>
2023-10-26 19:17:05 +0000

    Updated New File

---
+++ Working/01_04_conversionOperations.js
@@ -0,0 +1,45 @@
+// let score = 33;
+// let score = "33abc";
+// let score = null;
+// let score = undefined;
+// let score = true;
+
+//Type Of can be used in both forms
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let convertedValue = Number(score);
+// console.log(typeof convertedValue);
+// console.log(convertedValue);
+
+// "33" => 33
+// 33abc => Nan
+// true => 1,false -> 0
+
+//Boolean Conversion
+
+// let mybool = 1;
+// let mybool = "Nehal";
+// let mybool = "";
+
+// console.log(typeof mybool);
+
+// let con_mybool = Boolean(mybool);
+// console.log(typeof con_mybool);
+// console.log(con_mybool);
+
+// 1 => true,0 => false
+// "" => false
+// "Nehal" => true
+
+
+//String Conversion
+
+let my_num = 33
+console.log(typeof my_num);
+
+let con_num = String(my_num);
+console.log(typeof con_num);
+console.log(con_num);
+
+
